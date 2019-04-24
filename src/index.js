c823db690c19834d83d21d65c7e31dc1cbf75a1e
Eliot Szwajkowski <eliotpszw@gmail.com>
2019-04-24 18:06:59 -0400

    Example complete.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import SomeModule from './some-module.js';
+
+const myModule = new SomeModule('Eliot');
+
+myModule.sayHello();
+
---
+++ src/some-module.js
@@ -0,0 +1,11 @@
+class SomeModule {
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHello() {
+    console.log(`${this.name} says hello!`);
+  }
+}
+
+export default SomeModule;
